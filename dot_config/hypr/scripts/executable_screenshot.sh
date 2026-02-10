d93256edd20f8f216523ed0b9b15bee09ecf0970
linux_name2 <linux2@email.org>
2026-02-10 16:01:16 +0300

    screenshot script comments rewrite

--- dot_config/hypr/scripts/executable_screenshot.sh
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# Take a screenshot of a selected region and open it in swappy for editing.
+# Dependencies: slurp, grim, swappy
 
-# Запускаем slurp и сохраняем геометрию в переменную
+# Run slurp to select a region and save the geometry
 GEOMETRY=$(slurp)
 
-# Если геометрия пустая , выходим
+# Exit if no region was selected
 if [ -z "$GEOMETRY" ]; then
     exit 1
 fi
 
-# Делаем скриншот и передаем в swappy
+# Capture the screenshot and pipe it to swappy
 grim -g "$GEOMETRY" - | swappy -f -
