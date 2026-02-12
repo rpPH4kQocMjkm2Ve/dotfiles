a88bf00174e807ae20ab32da3e46e3dae9848b47
linux_name2 <linux2@email.org>
2026-02-13 00:40:44 +0300

    lib & wrappers rewrite, new functions

--- dot_local/bin/executable_makepkg-sandbox.sh
+++ dot_local/bin/executable_makepkg-sandbox.sh
@@ -5,29 +5,15 @@ set -euo pipefail
 
 BUILD_DIR="$(realpath "$PWD")"
 
-A=(
-    --ro-bind /usr /usr
-    --symlink /usr/bin /bin
-    --symlink /usr/bin /sbin
-    --symlink /usr/lib /lib
-    --proc /proc
-    --dev /dev
-    --tmpfs /tmp
-    --ro-bind /etc /etc
-    --ro-bind /var/lib/pacman /var/lib/pacman
-    --ro-bind /var/cache/pacman /var/cache/pacman
-    --tmpfs "${HOME}"
-    --bind "${BUILD_DIR}" "${BUILD_DIR}"
-)
-
+A=()
+bwrap_base A
 bwrap_lib64 A
 bwrap_resolv A
-
-A+=(--setenv HOME "${HOME}"
-    --setenv PATH "/usr/bin:/bin"
-    --setenv LANG "${LANG:-en_US.UTF-8}"
-    --unshare-all
-    --share-net
-    --die-with-parent)
+A+=(--ro-bind /var/lib/pacman /var/lib/pacman
+    --ro-bind /var/cache/pacman /var/cache/pacman
+    --tmpfs "${HOME}"
+    --bind "${BUILD_DIR}" "${BUILD_DIR}")
+bwrap_env_base A
 bwrap_hardened_malloc A default
+bwrap_sandbox A yes no
 exec bwrap "${A[@]}" -- /usr/bin/makepkg "$@"
