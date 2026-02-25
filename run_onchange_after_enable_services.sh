a18a511b8ba02ee7fef10d915cd5e81d15ea234a
linux_name2 <linux2@email.org>
2026-02-25 17:26:24 +0300

    sandbox: bwrap wrappers for nextcloud, fcitx5, otd-daemon, sparrow, transformers_ocr

--- run_onchange_after_enable_services.sh
+++ run_onchange_after_enable_services.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "--- User services ---"
 systemctl --user enable hypridle hyprpaper hyprpolkitagent hyprsunset waybar mpd
-systemctl --user enable com.nextcloud.desktopclient.nextcloud
 
 echo "--- System services ---"
 sudo systemctl enable firewalld
