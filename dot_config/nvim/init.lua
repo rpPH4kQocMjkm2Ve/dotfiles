66a93b9b136b7337f827a590143eccd8ab85f4e6
linux_name2 <linux2@email.org>
2026-03-13 16:08:40 +0300

    nvim: add indentation settings and auto-trim trailing whitespace on save

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,5 +5,22 @@ vim.cmd([[let g:loaded_matchparen = 1]])
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require("config.lazy")
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 
+vim.opt.fixendofline = true
+vim.opt.endofline = true
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*',
+    callback = function()
+        local pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[%s/\s\+$//e]])
+        vim.cmd([[%s/\n\+\%$//e]])
+        pcall(vim.api.nvim_win_set_cursor, 0, pos)
+    end,
+})
+
+require("config.lazy")
