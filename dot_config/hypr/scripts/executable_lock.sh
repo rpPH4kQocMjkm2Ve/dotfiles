2950491e1c265409538387608fb52cdf429c26d0
linux_name2 <linux2@email.org>
2026-03-30 15:46:25 +0300

    feat: add keys-vault (gocryptfs FBE for ~/keys) with auto open/close on login/lock

--- dot_config/hypr/scripts/executable_lock.sh
+++ dot_config/hypr/scripts/executable_lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set_usb_lock() {
-    state=$1
+    local state=$1
     for device in /sys/bus/usb/devices/usb*/authorized_default; do
         if [ -w "$device" ]; then
             echo "$state" > "$device"
@@ -9,10 +9,11 @@ set_usb_lock() {
     done
 }
 
+ssh-add -D
+keys-vault close
 set_usb_lock 0
-
 hyprlock
 
 set_usb_lock 1
-
+keys-vault open
 systemctl --user restart hyprsunset
