e3fb4cc7f2b347298db10994a5e0a8252c57b0fb
linux_name2 <linux2@email.org>
2026-03-30 23:17:14 +0300

    docs: add explanation about system services

--- run_onchange_after_enable_services.sh
+++ run_onchange_after_enable_services.sh
@@ -4,5 +4,7 @@ set -e
 echo "--- User services ---"
 systemctl --user enable hypridle hyprpaper hyprpolkitagent hyprsunset waybar mpd ssh-agent keys-vault ssh-add
 
+# System services live here (not in system-config aka root-chezmoi) to keep all service
+# enablement in one place and avoid requiring a separate apply step.
 echo "--- System services ---"
 sudo systemctl enable firewalld systemd-oomd
